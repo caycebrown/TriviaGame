7127cdf2284544bb14478fbd775c99aa3188ba2c
CBrown <47484591+caycebrown@users.noreply.github.com>
2019-04-03 16:04:55 -0500

    added a question array with question objects

---
+++ assets/javascript/app.js
@@ -0,0 +1,30 @@
+//qBank(question bank) array with question objects containing questions and answers------------------
+
+qBank = [
+    q1 = {
+        question : "This is question 1",
+        correct : "This is correct answer 1",
+        wrong1 : "this is wrong answer 1",
+        wrong2 : "this is wrong answer 2",
+        wrong3 : "this is wrong answer 3",
+    },
+
+    q2 = {
+        question : "This is question 2",
+        correct : "This is correct answer 1",
+        wrong1 : "this is wrong answer 1",
+        wrong2 : "this is wrong answer 2",
+        wrong3 : "this is wrong answer 3",
+    },
+
+    q3 = {
+        question : "This is question 1",
+        correct : "This is correct answer 1",
+        wrong1 : "this is wrong answer 1",
+        wrong2 : "this is wrong answer 2",
+        wrong3 : "this is wrong answer 3",
+    }
+]
+
+
+    
