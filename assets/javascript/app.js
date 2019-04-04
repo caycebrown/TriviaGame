0d4b6459c5bb0385962a5cfa52bd057372262370
CBrown <47484591+caycebrown@users.noreply.github.com>
2019-04-04 11:11:33 -0500

    added an html button to start the functions loop for game also added 2 js functions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,10 +3,11 @@
 qBank = [
     q1 = {
         question : "This is question 1",
-        correct : "This is correct answer 1",
+        correct : "This matches the answer",
         wrong1 : "this is wrong answer 1",
         wrong2 : "this is wrong answer 2",
         wrong3 : "this is wrong answer 3",
+        answer : "this is the answer",
     },
 
     q2 = {
@@ -24,7 +25,21 @@ qBank = [
         wrong2 : "this is wrong answer 2",
         wrong3 : "this is wrong answer 3",
     }
-]
+];
 
+i = 0;
 
-    
+function showQuestion(){
+    $('#card').html('<p>' + qBank[0] + '</p>')
+    setTimeout(answerTimer, 15000);
+    i++;
+
+};
+
+showQuestion();
+
+function answerTimer(){
+    $('#card').html('<p>THIS SAYS YAY YOURE CORRECT OR BOO YOURE INCORRECT</p>')
+};
+
+var test = $('button').attr('id')
