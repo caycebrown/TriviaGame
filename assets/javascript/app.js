bcc1b4530ce35e05239850d984b81de9509ffe0b
CBrown <47484591+caycebrown@users.noreply.github.com>
2019-04-07 18:19:51 -0500

    fixed syntax error with if else statement for evaluating user answer selection

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,7 +3,7 @@
 qBank = [
     q1 = {
         question : "This is question 1",
-        a1 : "This matches the answer",
+        a1 : "this is the answer",
         a2 : "this is wrong answer 1",
         a3 : "this is wrong answer 2",
         a4 : "this is wrong answer 3",
@@ -12,7 +12,7 @@ qBank = [
 
     q2 = {
         question : "This is question 2",
-        a1 : "This is a1 answer 1",
+        a1 : "This is the answer",
         a2 : "this is wrong answer 1",
         a3 : "this is wrong answer 2",
         a4 : "this is wrong answer 3",
@@ -20,8 +20,8 @@ qBank = [
     },
 
     q3 = {
-        question : "This is question 1",
-        a1 : "This is a1 answer 1",
+        question : "This is question 3",
+        a1 : "This is the answer",
         a2 : "this is wrong answer 1",
         a3 : "this is wrong answer 2",
         a4 : "this is wrong answer 3",
@@ -33,27 +33,47 @@ i = 0;
 
 var qItem = '<div class="row qItem">'
 
-function showQuestion(){
-    $('#card').append('<div class = "row" id="question">' + qBank[0].question + '</div>')
-    $('#card').append(qItem + qBank[0].a1 + '</div>')
-    $('#card').append(qItem + qBank[0].a2 + '</div>')
-    $('#card').append(qItem + qBank[0].a3 + '</div>')
-    $('#card').append(qItem + qBank[0].a4 + '</div>')
-    setTimeout(answerTimer, 40000);
-    i++;
+input = "";
+answer = qBank[i].answer;
 
+function showQuestion(){
+    $('#card').html('');//clears out element to prep for new questions and answers
+    $('#card').append('<div class = "row" id="question">' + qBank[i].question + '</div>')
+    $('#card').append(qItem + qBank[i].a1 + '</div>')
+    $('#card').append(qItem + qBank[i].a2 + '</div>')
+    $('#card').append(qItem + qBank[i].a3 + '</div>')
+    $('#card').append(qItem + qBank[i].a4 + '</div>')
+    answer = qBank[i].answer;
+    setTimeout(answerTimer, 10000);
+    
 };
+
+function correct() {
+    $('.selected').css('background-color', 'green' )
+    i++;
+    setTimeout(showQuestion, 3000);
+};
+
+function incorrect() {
+    $('.selected').css('background-color', 'red' )
+    i++;
+    setTimeout(showQuestion, 3000);  
+};
+
+function answerTimer(){
+    if (input === answer) {
+        correct();
+    } else {
+        incorrect();
+    }
+};
+
 
 showQuestion();
 
-function answerTimer(){
-    $('#card').html('<div class="row">THIS SAYS YAY YOURE a1 OR BOO YOURE INa1</div>')
-};
-
-var test = $('button').attr('id')
-
-var input = '';
 
 $('.qItem').on('click', function(){
+    $('.qItem').removeClass('selected'); //clears out the 'selected' class from the previous choice
+    $(this).addClass('selected');//Adds 'selected' class to current selection
     input = $(this).text();
 })
