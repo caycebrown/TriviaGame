c2d80e988248cf07e085c4aa9ac2c196fd3a55dc
CBrown <47484591+caycebrown@users.noreply.github.com>
2019-04-09 12:27:25 -0500

    added real questions and answers in place of testing q and a content, also added a timer to notify user of remaining time to answer question

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,30 +1,66 @@
 //qBank(question bank) array with question objects containing questions and answers------------------
 qBank = [
     q1 = {
-        question : "This is question 1",
-        a1 : "this is the answer",
-        a2 : "this is wrong answer 1",
-        a3 : "this is wrong answer 2",
-        a4 : "this is wrong answer 3",
-        answer : "this is the answer",
+        question : "According to modern day estimates the Roman Colesseum seated this number of people.",
+        a1 : "50,000",
+        a2 : "27,000",
+        a3 : "18,000",
+        a4 : "80,000",
+        answer : "50,000",
     },
 
     q2 = {
-        question : "This is question 2",
-        a1 : "this is the answer 2",
-        a2 : "this is wrong answer 1",
-        a3 : "this is wrong answer 2",
-        a4 : "this is wrong answer 3",
-        answer : "this is the answer 2",
+        question : "Christ the Redeemer in Rio De Janeiro sits atop this mountain.",
+        a1 : "Pedra da Mina",
+        a2 : "Sugarloaf Mountain",
+        a3 : "Mount Roraima",
+        a4 : "Mount Corvocado",
+        answer : "Mount Corvocado",
     },
 
     q3 = {
-        question : "This is question 3",
-        a1 : "this is answer 3",
-        a2 : "this is wrong answer 1",
-        a3 : "this is wrong answer 2",
-        a4 : "this is wrong answer 3",
-        answer : "this is answer 3",
+        question : "The Great Wall of China - including all of its branches - is how long?",
+        a1 : "8,769 Miles",
+        a2 : "13,171 Miles",
+        a3 : "4,515 Miles",
+        a4 : "17,846 Miles",
+        answer : "13,171 Miles",
+    },
+
+    q4 = {
+        question : "The ancient city of Petra in Jordan is sometimes called what?",
+        a1 : "The City of Stone",
+        a2 : "The Rose City",
+        a3 : "The Red City",
+        a4 : "The Salt City",
+        answer : "The Rose City",
+    },
+
+    q5 = {
+        question : "Which of these structures does NOT exist in the Mayan city of Chichen Itza?",
+        a1 : "The Temple of Water",
+        a2 : "The Great Ball Court",
+        a3 : "The Temple of Jaguars",
+        a4 : "The Kukulkan Pyramid",
+        answer : "The Temple of Water",
+    },
+
+    q6 = {
+        question : "The Taj Mahal was constructed to be what?",
+        a1 : "A Mosque",
+        a2 : "The ending site of a popular pilgrimage",
+        a3 : "A tribute to the Mughal emperor, Shah Jahan",
+        a4 : "A Mausoleum",
+        answer : "A Mausoleum",
+    },
+
+    q7 = {
+        question : "Machu Piccu was home to what ancient civilization?",
+        a1 : "The Aztecs",
+        a2 : "The Incas",
+        a3 : "The Teotihuacans",
+        a4 : "The Purépechas",
+        answer : "The Incas",
     }
 ];
 
@@ -34,6 +70,8 @@ var wrong = 0;
 var qItem = '<div class="row qItem">'
 var input = "";
 var answer = qBank[i].answer;
+var time = 30;
+
 
 function showQuestion(){
     $('#card').html('');//clears out element to prep for new questions and answers
@@ -43,16 +81,24 @@ function showQuestion(){
     $('#card').append(qItem + qBank[i].a3 + '</div>')
     $('#card').append(qItem + qBank[i].a4 + '</div>')
     answer = qBank[i].answer;
-    setTimeout(answerTimer, 10000);
-    
+
+    setTimeout(answerTimer, 31001);
+
+    intervalId = setInterval(countDown, 1000);
+    function countDown(){
+        $('#time').html('Time: &nbsp;' + time);
+        time--;
+    }
 };
 
 function correct() {
+    clearInterval(intervalId);
     $('.selected').css('background-color', 'green' )
     i++;
     right++;
     if ( i < qBank.length) {
-        setTimeout(showQuestion, 3000);
+        setTimeout(showQuestion, 6000);
+        time = 30;
     } else {
         results();
     }
@@ -60,12 +106,14 @@ function correct() {
 };
 
 function incorrect() {
+    clearInterval(intervalId);
     $('.selected').css('background-color', 'red' )
-    $('#card').append(qItem + 'Correct Answer: ' + qBank[i].answer + ' </div>')
+    $('#card').append('<div><h2>Correct answer: ' + qBank[i].answer + '</h2> </div>')
     i++;
     wrong++;
     if ( i < qBank.length) {
-        setTimeout(showQuestion, 3000);
+        setTimeout(showQuestion, 6000);
+        time = 30;
     } else {
         results();
     }
@@ -97,3 +145,5 @@ $('#card').on('click', '.qItem', function(){
     $(this).addClass('selected');//Adds 'selected' class to current selection
     input = $(this).text();
 })
+
+
